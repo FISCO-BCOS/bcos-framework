f5cf1fde68e586bdd0d2409da04863f0559efb1d
cyjseagull <1547158861@qq.com>
2021-03-16 14:47:29 +0800

    add codec for ContractABI (#15)

--- cmake/InstallInclude.cmake
+++ cmake/InstallInclude.cmake
@@ -1,11 +1,20 @@
+# utilities
 install(
     DIRECTORY "libutilities/"
     DESTINATION "${CMAKE_INSTALL_INCLUDEDIR}/bcos-framework/libutilities"
     FILES_MATCHING PATTERN "*.h"
 )
 
+# interfaces
 install(
     DIRECTORY "interfaces"
     DESTINATION "${CMAKE_INSTALL_INCLUDEDIR}/bcos-framework"
+    FILES_MATCHING PATTERN "*.h"
+)
+
+# codec
+install(
+    DIRECTORY "libcodec"
+    DESTINATION "${CMAKE_INSTALL_INCLUDEDIR}/bcos-framework/libcodec"
     FILES_MATCHING PATTERN "*.h"
 )
