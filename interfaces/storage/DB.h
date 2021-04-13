5c5361c637bfb9b8b74bf851cece1a4d236a2053
bxq2011hust <bxq2011hust@qq.com>
2021-04-13 11:22:44 +0800

    add storage interfaces

---
+++ interfaces/storage/DB.h
@@ -0,0 +1,64 @@
+/*
+ *  Copyright (C) 2021 FISCO BCOS.
+ *  SPDX-License-Identifier: Apache-2.0
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ * @brief interface of DB
+ * @file DB.h
+ * @author: xingqiangbai
+ * @date: 2021-04-07
+ */
+#pragma once
+#include "Table.h"
+#include "bcos-framework/libutilities/Error.h"
+#include <functional>
+
+namespace bcos
+{
+namespace storage
+{
+struct Query : public std::enable_shared_from_this<Query>
+{
+    using Ptr = std::shared_ptr<Query>;
+    shared_ptr<TableInfo> tableInfo;
+    shared_ptr<Condition> condition;
+};
+
+class DB : public std::enable_shared_from_this<DB>
+{
+public:
+    using Ptr = std::shared_ptr<DB>;
+    DB() = default;
+    virtual ~DB() {}
+    virtual std::vector<string> getPrimaryKeys(std::shared_ptr<Query>& _query) = 0;
+    virtual std::shared_ptr<Entry> getRow(
+        std::shared_ptr<TableInfo>& _tableInfo, const std::string_view& _key) = 0;
+    virtual std::map<std::string, std::shared_ptr<Entry>> getRows(
+        std::shared_ptr<TableInfo>& _tableInfo, const std::vector<std::string_view>& _keys) = 0;
+    virtual size_t commitTables(
+        const map<TableInfo, std::map<std::string, std::shared_ptr<Entry>>>& _data) = 0;
+
+    virtual void asyncGetPrimaryKeys(std::shared_ptr<Query>& _query,
+        std::function<void(Error, std::vector<std::string> >)> _callback) = 0;
+    virtual void asyncGetRow(std::shared_ptr<TableInfo>& _tableInfo, const string_view& _key,
+        std::function<void(Error, std::shared_ptr<Entry> >)> _callback) = 0;
+    virtual void asyncGetRows(std::shared_ptr<TableInfo>& _tableInfo,
+        const std::vector<std::string>& _keys,
+        std::function<void(Error, std::map<std::string, std::shared_ptr<Entry>>)> _callback) = 0;
+    virtual void asyncCommitTables(
+        const std::map<TableInfo, std::map<std::string, std::shared_ptr<Entry>>>& _data,
+        std::function<void(Error)> _callback) = 0;
+};
+
+}  // namespace storage
+}  // namespace bcos
---
+++ interfaces/storage/Table.h
@@ -0,0 +1,137 @@
+/*
+ *  Copyright (C) 2021 FISCO BCOS.
+ *  SPDX-License-Identifier: Apache-2.0
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ * @brief interface of Table
+ * @file Table.h
+ * @author: xingqiangbai
+ * @date: 2021-04-07
+ */
+#pragma once
+#include "interfaces/crypto/CommonType.h"
+#include "interfaces/protocol/Block.h"
+#include <map>
+#include <memory>
+#include <string>
+#include <vector>
+
+namespace bcos
+{
+namespace storage
+{
+class Entry
+{
+public:
+    using Ptr = std::shared_ptr<Entry>;
+    using ConstPtr = std::shared_ptr<const Entry>;
+    Entry() = default;
+    virtual ~Entry() {}
+
+    virtual std::string getField(const std::string_view& key) const = 0;
+    virtual std::string_view getFieldConst(const std::string_view& key) const = 0;
+    virtual void setField(const std::string_view& key, const std::string_view& value) = 0;
+    virtual std::map<std::string, std::string>::const_iterator find(
+        const std::string& key) const = 0;
+    virtual std::map<std::string, std::string>::const_iterator begin() const = 0;
+    virtual std::map<std::string, std::string>::const_iterator end() const = 0;
+    virtual size_t size() const = 0;
+    virtual bool isDeleted() const = 0;
+    virtual void setDeleted(bool status) = 0;
+    virtual ssize_t capacity() const = 0;
+
+    virtual int64_t num() const = 0;
+    virtual void setNum(int64_t num) = 0;
+
+    virtual void copyFrom(Entry::ConstPtr entry);
+
+    // TODO: check if we still need these methods
+    virtual int64_t getID() const = 0;
+    virtual void setID(int64_t id) = 0;
+    virtual void setID(const std::string_view& id) = 0;
+    virtual bool dirty() const = 0;
+    virtual void setDirty(bool dirty) = 0;
+    virtual ssize_t capacityOfHashField() const = 0;
+    virtual ssize_t refCount() const = 0;  // for test?
+};
+
+class Condition : public std::enable_shared_from_this<Condition>
+{
+public:
+    using Ptr = std::shared_ptr<Condition>;
+    virtual void EQ(const std::string& key, const std::string& value) = 0;
+    virtual void NE(const std::string& key, const std::string& value) = 0;
+
+    virtual void GT(const std::string& key, const std::string& value) = 0;
+    virtual void GE(const std::string& key, const std::string& value) = 0;
+
+    virtual void LT(const std::string& key, const std::string& value) = 0;
+    virtual void LE(const std::string& key, const std::string& value) = 0;
+
+    virtual bool isValid(const std::string_view& key) const = 0;
+};
+
+struct TableInfo : public std::enable_shared_from_this<TableInfo>
+{
+    using Ptr = std::shared_ptr<TableInfo>;
+    std::string name;
+    std::string key;
+    std::vector<std::string> fields;
+    std::vector<Address> authorizedAddress;
+    std::vector<std::string> indices;
+
+    bool enableConsensus = true;
+    bool enableCache = true;
+};
+
+class Table : public std::enable_shared_from_this<Table>
+{
+public:
+    using Ptr = std::shared_ptr<Table>;
+    Table() = default;
+    virtual ~Table() {}
+    virtual std::shared_ptr<Entry> getRow(const std::string_view& _key) = 0;
+    virtual std::map<std::string, std::shared_ptr<Entry>> getRows(
+        const std::vector<std::string>& _keys) = 0;
+    virtual std::vector<std::string> getPrimaryKeys(std::shared_ptr<Condition> _condition) = 0;
+    virtual bool setRow(const std::string_view& _key, std::shared_ptr<Entry> _entry) = 0;
+    virtual bool remove(const std::string_view& _key) = 0;
+    virtual HashType hash() = 0;
+    virtual TableInfo::Ptr tableInfo() = 0;
+
+    virtual bool checkAuthority(Address const& _origin) const = 0;
+    virtual std::map<std::string, std::shared_ptr<Entry>> dump() = 0;
+
+    virtual void rollback(const Change& _change) = 0;
+    virtual void setStateStorage(std::shared_ptr<Storage> _db) = 0;
+};
+
+class TableFactory : public std::enable_shared_from_this<TableFactory>
+{
+public:
+    using Ptr = std::shared_ptr<TableFactory>;
+    TableFactory() = default;
+    virtual ~TableFactory() {}
+    virtual void init() = 0;
+
+    virtual shared_ptr<Table> openTable(const std::string& _tableName, bool _authority = true) = 0;
+    virtual bool createTable(const std::string& _tableName, const std::string& _keyField,
+        const std::string& _valueFields) = 0;
+
+    virtual HashType hash() = 0;
+    virtual size_t savepoint() = 0;
+    virtual void rollback(size_t _savepoint) = 0;
+    virtual void commit(protocol::Block::Ptr _block) = 0;
+};
+}  // namespace storage
+}  // namespace bcos
