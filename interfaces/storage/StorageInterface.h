436bf2fec5fab1b46a3628db4704086e76552a81
Xingqang Bai <bxq2011hust@qq.com>
2021-04-26 21:17:15 +0800

    fix threadPool compile failed (#76)

--- interfaces/storage/StorageInterface.h
+++ interfaces/storage/StorageInterface.h
@@ -44,19 +44,19 @@ public:
     virtual std::vector<std::string> getPrimaryKeys(
         std::shared_ptr<TableInfo> _tableInfo, std::shared_ptr<Condition> _condition) const = 0;
     virtual std::shared_ptr<Entry> getRow(
-        std::shared_ptr<TableInfo>& _tableInfo, const std::string_view& _key) = 0;
+        std::shared_ptr<TableInfo> _tableInfo, const std::string_view& _key) = 0;
     virtual std::map<std::string, std::shared_ptr<Entry>> getRows(
-        std::shared_ptr<TableInfo>& _tableInfo, const std::vector<std::string>& _keys) = 0;
+        std::shared_ptr<TableInfo> _tableInfo, const std::vector<std::string>& _keys) = 0;
     virtual size_t commitTables(const std::vector<std::shared_ptr<TableInfo>> _tableInfos,
         std::vector<std::shared_ptr<std::map<std::string, std::shared_ptr<Entry>>>>&
             _tableDatas) = 0;
 
-    virtual void asyncGetPrimaryKeys(std::shared_ptr<TableInfo>& _tableInfo,
+    virtual void asyncGetPrimaryKeys(std::shared_ptr<TableInfo> _tableInfo,
         std::shared_ptr<Condition> _condition,
         std::function<void(Error, std::vector<std::string>)> _callback) const = 0;
-    virtual void asyncGetRow(std::shared_ptr<TableInfo>& _tableInfo, const std::string_view& _key,
+    virtual void asyncGetRow(std::shared_ptr<TableInfo> _tableInfo, const std::string_view& _key,
         std::function<void(Error, std::shared_ptr<Entry>)> _callback) = 0;
-    virtual void asyncGetRows(std::shared_ptr<TableInfo>& _tableInfo,
+    virtual void asyncGetRows(std::shared_ptr<TableInfo> _tableInfo,
         const std::vector<std::string>& _keys,
         std::function<void(Error, std::map<std::string, std::shared_ptr<Entry>>)> _callback) = 0;
     virtual void asyncCommitTables(const std::vector<std::shared_ptr<TableInfo>> _tableInfos,
@@ -76,10 +76,10 @@ public:
     virtual std::shared_ptr<TableFactory> getStateCache(protocol::BlockNumber _blockNumber) = 0;
 
     // KV store in split database, used to store data off-chain
-    virtual bool put(const std::string_view& columnFamily, const std::string_view& key,
+    virtual bool put(const std::string& columnFamily, const std::string_view& key,
         const std::string_view& value) = 0;
-    virtual std::string get(const std::string_view& columnFamily, const std::string_view& key) = 0;
-    virtual void asyncGetBatch(const std::string_view& columnFamily,
+    virtual std::string get(const std::string& columnFamily, const std::string_view& key) = 0;
+    virtual void asyncGetBatch(const std::string& columnFamily,
         std::shared_ptr<std::vector<std::string_view>> keys,
         std::function<void(Error, std::shared_ptr<std::vector<std::string>>)> callback) = 0;
 };
