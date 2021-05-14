ef2786838b54beea3149a2712111ade441ce814a
Xingqang Bai <bxq2011hust@qq.com>
2021-05-14 15:54:57 +0800

    add asyncCommit to TableFactory (#121)

--- test/unittests/libtable/TableFactory.cpp
+++ test/unittests/libtable/TableFactory.cpp
@@ -253,6 +253,7 @@ BOOST_AUTO_TEST_CASE(hash)
     BOOST_TEST(keys.size() == 1);
     entries = table->getRows(keys);
     BOOST_TEST(entries.size() == 1);
+    tableFactory->asyncCommit([](Error, size_t){});
 }
 
 BOOST_AUTO_TEST_CASE(parallel_openTable)
