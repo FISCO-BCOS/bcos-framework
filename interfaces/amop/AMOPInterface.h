c62c0b86b4b557716213c71efa0e47338bfbed77
octopus <912554887@qq.com>
2021-08-06 15:29:53 +0800

    modify asyncNotifyAmopMessage

--- interfaces/amop/AMOPInterface.h
+++ interfaces/amop/AMOPInterface.h
@@ -45,8 +45,8 @@ public:
      * @param _data: the message data
      * @return void
      */
-    virtual void asyncNotifyAmopMessage(
-        bcos::crypto::NodeIDPtr _nodeID, const std::string& _id, bcos::bytesConstRef _data) = 0;
+    virtual void asyncNotifyAmopMessage(bcos::crypto::NodeIDPtr _nodeID, const std::string& _id,
+        bcos::bytesConstRef _data, std::function<void(Error::Ptr _error)> _onRecv) = 0;
     /**
      * @brief: async receive nodeIDs from front service
      * @param _nodeIDs: the nodeIDs
