bf4b2599fbd276ad01fb0c5e6468d72eb9015248
morebtcg <651932351@qq.com>
2021-08-23 09:39:35 +0800

    add TarsLog impl

---
+++ libutilities/TarsLog.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include "servant/TarsLogger.h"
+#include <iostream>
+
+#define LOG_BADGE(_NAME) "[" << (_NAME) << "]"
+#define LOG_TYPE(_TYPE) (_TYPE) << "|"
+#define LOG_DESC(_DESCRIPTION) (_DESCRIPTION)
+#define LOG_KV(_K, _V) "," << (_K) << "=" << (_V)
+
+#define TRACE TarsRollLogger::DEBUG_LOG
+#define DEBUG TarsRollLogger::DEBUG_LOG
+#define INFO TarsRollLogger::INFO_LOG
+#define WARNING TarsRollLogger::WARN_LOG
+#define ERROR TarsRollLogger::ERROR_LOG
+#define FATAL TarsRollLogger::ERROR_LOG
+
+template <class Logger>
+class EndlHelper
+{
+public:
+    EndlHelper(Logger& logger) : m_logger(logger) {}
+
+    template <class Message>
+    EndlHelper& operator<<(Message message)
+    {
+        m_logger << message;
+        return *this;
+    }
+
+    ~EndlHelper() { m_logger << std::endl; }
+
+private:
+    Logger& m_logger;
+};
+
+#define BCOS_LOG(level)        \
+    if (LOG->IsNeedLog(level)) \
+    EndlHelper(LOG->log(level))
+#define BCOS_STAT_LOG(level)   \
+    if (LOG->IsNeedLog(level)) \
+    EndlHelper(LOG->log(level))
