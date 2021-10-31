3cee635d54f6f28ad60bc373b10dbb75959b69e5
MO NAN <651932351@qq.com>
2021-10-31 11:32:12 +0800

    Move TransactionStatus operator<< to header (#363)

--- libprotocol/TransactionStatus.h
+++ libprotocol/TransactionStatus.h
@@ -21,6 +21,7 @@
 #pragma once
 #include <iostream>
 #include <sstream>
+
 namespace bcos
 {
 struct Exception;
@@ -61,7 +62,7 @@ enum class TransactionStatus : int32_t
     InvalidSignature = 10008,
     RequestNotBelongToTheGroup = 10009,
 };
-std::ostream& operator<<(std::ostream& _out, TransactionStatus const& _er);
+
 inline std::string toString(protocol::TransactionStatus const& _i)
 {
     std::stringstream stream;
@@ -70,3 +71,97 @@ inline std::string toString(protocol::TransactionStatus const& _i)
 }
 }  // namespace protocol
 }  // namespace bcos
+
+namespace std
+{
+inline std::ostream& operator<<(std::ostream& _out, bcos::protocol::TransactionStatus const& _er)
+{
+    {
+        switch (_er)
+        {
+        case bcos::protocol::TransactionStatus::None:
+            _out << "None";
+            break;
+        case bcos::protocol::TransactionStatus::OutOfGasLimit:
+            _out << "OutOfGasLimit";
+            break;
+        case bcos::protocol::TransactionStatus::NotEnoughCash:
+            _out << "NotEnoughCash";
+            break;
+        case bcos::protocol::TransactionStatus::BadInstruction:
+            _out << "BadInstruction";
+            break;
+        case bcos::protocol::TransactionStatus::BadJumpDestination:
+            _out << "BadJumpDestination";
+            break;
+        case bcos::protocol::TransactionStatus::OutOfGas:
+            _out << "OutOfGas";
+            break;
+        case bcos::protocol::TransactionStatus::OutOfStack:
+            _out << "OutOfStack";
+            break;
+        case bcos::protocol::TransactionStatus::StackUnderflow:
+            _out << "StackUnderflow";
+            break;
+        case bcos::protocol::TransactionStatus::NonceCheckFail:
+            _out << "NonceCheckFail";
+            break;
+        case bcos::protocol::TransactionStatus::BlockLimitCheckFail:
+            _out << "BlockLimitCheckFail";
+            break;
+        case bcos::protocol::TransactionStatus::PrecompiledError:
+            _out << "PrecompiledError";
+            break;
+        case bcos::protocol::TransactionStatus::RevertInstruction:
+            _out << "RevertInstruction";
+            break;
+        case bcos::protocol::TransactionStatus::ContractAddressAlreadyUsed:
+            _out << "ContractAddressAlreadyUsed";
+            break;
+        case bcos::protocol::TransactionStatus::PermissionDenied:
+            _out << "PermissionDenied";
+            break;
+        case bcos::protocol::TransactionStatus::CallAddressError:
+            _out << "CallAddressError";
+            break;
+        case bcos::protocol::TransactionStatus::GasOverflow:
+            _out << "GasOverflow";
+            break;
+        case bcos::protocol::TransactionStatus::ContractFrozen:
+            _out << "ContractFrozen";
+            break;
+        case bcos::protocol::TransactionStatus::AccountFrozen:
+            _out << "AccountFrozen";
+            break;
+        case bcos::protocol::TransactionStatus::TxPoolIsFull:
+            _out << "TxPoolIsFull";
+            break;
+        case bcos::protocol::TransactionStatus::Malform:
+            _out << "MalformTx";
+            break;
+        case bcos::protocol::TransactionStatus::AlreadyInTxPool:
+            _out << "AlreadyInTxPool";
+            break;
+        case bcos::protocol::TransactionStatus::TxAlreadyInChain:
+            _out << "TxAlreadyInChain";
+            break;
+        case bcos::protocol::TransactionStatus::InvalidChainId:
+            _out << "InvalidChainId";
+            break;
+        case bcos::protocol::TransactionStatus::InvalidGroupId:
+            _out << "InvalidGroupId";
+            break;
+        case bcos::protocol::TransactionStatus::InvalidSignature:
+            _out << "InvalidSignature";
+            break;
+        case bcos::protocol::TransactionStatus::RequestNotBelongToTheGroup:
+            _out << "RequestNotBelongToTheGroup";
+            break;
+        default:
+            _out << "Unknown";
+            break;
+        }
+        return _out;
+    }
+}
+}  // namespace std
