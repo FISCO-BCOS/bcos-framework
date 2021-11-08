b8d7ddf01b0b09197551956b7a9c918fa58666a3
Kyon <32325790+kyonRay@users.noreply.github.com>
2021-11-08 10:46:11 +0800

    <fix>(interface): rename precompiled address. (#378)

--- test/unittests/faker/FakersTest.cpp
+++ test/unittests/faker/FakersTest.cpp
@@ -77,13 +77,12 @@ BOOST_AUTO_TEST_CASE(fakeTxPoolConstructor)
     std::make_shared<FakeTxPool>();
 
     std::cout << bcos::precompiled::SYS_CONFIG_NAME << std::endl;
-    std::cout << bcos::precompiled::CRUD_NAME << std::endl;
-    std::cout << bcos::precompiled::TABLE_FACTORY_NAME << std::endl;
+    std::cout << bcos::precompiled::TABLE_NAME << std::endl;
     std::cout << bcos::precompiled::CONSENSUS_NAME << std::endl;
     std::cout << bcos::precompiled::CNS_NAME << std::endl;
     std::cout << bcos::precompiled::PARALLEL_CONFIG_NAME << std::endl;
     std::cout << bcos::precompiled::CONTRACT_LIFECYCLE_NAME << std::endl;
-    std::cout << bcos::precompiled::KV_TABLE_FACTORY_NAME << std::endl;
+    std::cout << bcos::precompiled::KV_TABLE_NAME << std::endl;
     std::cout << bcos::precompiled::CRYPTO_NAME << std::endl;
     std::cout << bcos::precompiled::DAG_TRANSFER_NAME << std::endl;
     std::cout << bcos::precompiled::CONTRACT_AUTH_NAME << std::endl;
@@ -91,15 +90,14 @@ BOOST_AUTO_TEST_CASE(fakeTxPoolConstructor)
     std::cout << bcos::precompiled::BFS_NAME << std::endl;
 
     std::cout << bcos::precompiled::SYS_CONFIG_ADDRESS << std::endl;
-    std::cout << bcos::precompiled::TABLE_FACTORY_ADDRESS << std::endl;
+    std::cout << bcos::precompiled::TABLE_ADDRESS << std::endl;
     std::cout << bcos::precompiled::CONSENSUS_ADDRESS << std::endl;
-    std::cout << bcos::precompiled::CRUD_ADDRESS << std::endl;
     std::cout << bcos::precompiled::CNS_ADDRESS << std::endl;
     std::cout << bcos::precompiled::CONTRACT_AUTH_ADDRESS << std::endl;
     std::cout << bcos::precompiled::PARALLEL_CONFIG_ADDRESS << std::endl;
     std::cout << bcos::precompiled::CONTRACT_LIFECYCLE_ADDRESS << std::endl;
     std::cout << bcos::precompiled::CHAIN_GOVERNANCE_ADDRESS << std::endl;
-    std::cout << bcos::precompiled::KV_TABLE_FACTORY_ADDRESS << std::endl;
+    std::cout << bcos::precompiled::KV_TABLE_ADDRESS << std::endl;
     std::cout << bcos::precompiled::CRYPTO_ADDRESS << std::endl;
     std::cout << bcos::precompiled::WORKING_SEALER_MGR_ADDRESS << std::endl;
     std::cout << bcos::precompiled::DAG_TRANSFER_ADDRESS << std::endl;
